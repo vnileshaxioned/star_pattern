e46b39fc430a293aedb2dd3c4f1ffda60bae2f75
Nilesh Vishwakarma <nileshv@axioned.com>
2022-04-20 19:23:07 +0530

    add  variable

--- star_pattern.php
+++ star_pattern.php
@@ -2,12 +2,13 @@
 
 echo "Print A <br><br>";
 
-for ($i = 0; $i <= 5; $i++) {
-    for($j = 0; $j <= 5; $j++) {
+$n = 5;
+for ($i = 0; $i <= $n; $i++) {
+    for($j = 0; $j <= $n; $j++) {
         if(
-            ($i != 0 and $i != 3 and $j > 0 and $j < 5)
-            or ($i == 0 and $j > 3)
-            or ($i == 0 and $j < 2)
+            ($i != 0 and $i != 3 and $j > 0 and $j < $n)
+            or ($i == 0 and $j > ($n -2))
+            or ($i == 0 and $j < ($n -3))
         ) {
             echo "&nbsp;&nbsp;";
         } else {
@@ -17,14 +18,17 @@ for ($i = 0; $i <= 5; $i++) {
     echo "<br>";
 }
 
+
+
 echo "<hr>";
 echo "Print P <br><br>";
 
-for ($i = 0; $i <= 5; $i++) {
-    for($j = 0; $j <= 5; $j++) {
+$n = 6;
+for ($i = 0; $i <= $n; $i++) {
+    for($j = 0; $j <= $n; $j++) {
         if(
-            ($i != 0 and $i != 3 and $j > 0 and $j < 5)
-            or ($i != 1 and $i != 2 and $j > 4)
+            ($i != 0 and $i != 3 and $j > 0 and $j < $n)
+            or ($i != 1 and $i != 2 and $j > ($n -1))
         ) {
             echo "&nbsp;&nbsp";
         } else {
